d8015516d5cbe94ad34e6c368b7b7deec209cf34
philiprzn <philip_bmx11@yahoo.com>
2018-03-11 21:59:11 +0300

    js_task2_05

--- js_task2_05.js
+++ js_task2_05.js
@@ -1,28 +1,29 @@
 //////////////////////////////////////////// js_task2_05:
 
-// var numArr = [1, 83, 76];
-//
-// function getMaxOfArray(numArray) {
-//     return Math.max.apply(null, numArray);
-// }
-//
-// console.log(getMaxOfArray(numArr));
+var numArr = [1, 83, 76];
+
+function getMaxOfArray(numArray) {
+    var max = Math.max.apply(null, numArray)
+    return ('Максимальное значение ' + max) ;
+}
+
+console.log(getMaxOfArray(numArr));
 
 //////////////////////////////////////////// js_task2_05_v2:
 
-var numArr = [1, 83, 76, 522, -25, 8452];
-
-
-
-min = numArr[0];
-max = min;
-
-numArr.forEach(function (item, i) {
-    // console.log(item);
-    if (numArr[i] > max) max = numArr[i];
-    if (numArr[i] < min) min = numArr[i];
-});
-
-console.log(numArr);
-console.log('Максимальное значение массива: ' + max);
-console.log('Минимальное значение массива: ' + min);
+// var numArr = [1, 83, 76, 522, -25, 8452];
+//
+//
+//
+// min = numArr[0];
+// max = min;
+//
+// numArr.forEach(function (item, i) {
+//     // console.log(item);
+//     if (numArr[i] > max) max = numArr[i];
+//     if (numArr[i] < min) min = numArr[i];
+// });
+//
+// console.log(numArr);
+// console.log('Максимальное значение массива: ' + max);
+// console.log('Минимальное значение массива: ' + min);
